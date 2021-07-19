15ccde86dd81f79725c9542ea32339e29928d134
Zachary Williams <kcaz311@gmail.com>
2021-07-19 15:29:44 -0400

    latest comic image showing

--- src/Components/Latest/Latest.js
+++ src/Components/Latest/Latest.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Latest extends Component {
+    state = {
+        latestXKCDimg: null
+    }
+    componentDidMount() {
+        fetch("https://xkcd.vercel.app/?comic=latest")
+            .then(res => res.json())
+            .then((result) => {
+                this.setState({
+                latestXKCDimg: result.img
+                })
+            })
+    }
+
     render() {
         return (
-            <div>
-                Latest
-            </div>
+                <div>
+                    <img src={this.state.latestXKCDimg} />
+                </div>
         );
     }
 }
